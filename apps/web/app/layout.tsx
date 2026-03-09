f7662b99c22077025a5076b9c0a0d28bd3ab1799
sic-em <bawskiya@gmail.com>
2026-03-09 01:33:57 -0500

    fix(web): enable system theme

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem={false}
+          enableSystem
           disableTransitionOnChange
         >
           <div className="mx-auto w-full max-w-7xl">
